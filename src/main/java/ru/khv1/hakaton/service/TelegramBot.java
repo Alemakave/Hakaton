af7524d2ce4383b14b50f9a41e4770b4936dd5c6
Alemakave <savercat4@gmail.com>
2023-11-03 22:07:03 +1000

    Создан работоспособный бот с командами

--- src/main/java/ru/khv1/hakaton/service/TelegramBot.java
+++ src/main/java/ru/khv1/hakaton/service/TelegramBot.java
@@ -1,19 +1,62 @@
 package ru.khv1.hakaton.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.khv1.hakaton.configs.BotConfig;
+import ru.khv1.hakaton.utils.ServerUtils;
 
 @Component
 @AllArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
-    private final BotConfig botConfig;
+    @Autowired
+    private BotConfig botConfig;
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
+            switch (messageText.toLowerCase()) {
+                case "/start":
+                    sendMessage(chatId, "Добро пожаловать!\n" +
+                            "Данный бот разработан для хакатона\n" +
+                            "Доступные комманды:\n" +
+                            "reboot/restart - перезапуск SQL сервера\n" +
+                            "clear          - отчистка дискового пространства");
+                    break;
+                case "restart":
+                case "reboot":
+                    ServerUtils.rebootServer();
+                    sendMessage(chatId, "Reboot");
+                    break;
+                case "clear":
+                    ServerUtils.clear();
+                    sendMessage(chatId, "Clear");
+                    break;
+                default:
+                    sendMessage(chatId, String.format("Команда %s не найдена", messageText));
+            }
+        }
+    }
+
+    private void sendMessage(Long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+            for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+                System.out.println(stackTraceElement);
+            }
+        }
     }
 
     @Override
---
+++ src/main/java/ru/khv1/hakaton/utils/ServerUtils.java
@@ -0,0 +1,6 @@
+package ru.khv1.hakaton.utils;
+
+public class ServerUtils {
+    public static void rebootServer() {}
+    public static void clear() {}
+}
